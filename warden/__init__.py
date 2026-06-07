4c5cfcd2213f2ae2363229c154b976e352d90576
ar9av <ar9avg@gmail.com>
2026-06-07 22:33:09 +0530

    chore: bump version to 1.6.0

--- warden/__init__.py
+++ warden/__init__.py
@@ -1,6 +1,6 @@
 """Prismor Warden local session-security utility."""
 
-__version__ = "1.5.8"
+__version__ = "1.6.0"
 
 from warden.semantic_guard import SemanticGuard, SemanticRisk
 from warden.semantic_guard_v2 import SemanticGuardV2, HybridRisk
